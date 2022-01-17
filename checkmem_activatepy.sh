cb9ba0a70b9d2a31bc5538da52257a15bbde7cf0
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-01-17 15:17:27 +0000

    Add files via upload

---
+++ checkmem_activatepy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#---------PERSONAL VARIABLES
+PCUSER=`whoami`
+RAM_PERCENTAGE_WARNING="1"
+HOME_DIRECTORY="/home/$PCUSER"
+TMP_DIRECTORY="/tmp"
+#---------SCRIPT - CHECK MEM
+USED_MEM=`vmstat -s |awk '/used/ && /memory/' |cut -d'K' -f1 | sed "s/ //g"`
+PC_MEM=`vmstat -s |awk '/total/ && /memory/' |cut -d'K' -f1 | sed "s/ //g"`
+RATIO_WARNING=`echo "scale=2; $USED_MEM / $PC_MEM * 100" | bc | cut -d'.' -f1`
+#--------SCRIPT - SEND EMAIL       
+if [[ "$RATIO_WARNING" -ge $RAM_PERCENTAGE_WARNING  && ! -f $TMP_DIRECTORY/nowarning.txt ]]
+then
+	cd $HOME_DIRECTORY/.scriptmail-dependencies/
+	python3 mailwarningmem.py
+	cd $TMP_DIRECTORY
+	echo 0 > nowarning.txt
+fi
---
+++ sendmailwarningmem.py
@@ -0,0 +1,15 @@
+import base64
+import smtplib
+sender_email = "///"
+password_base64 = "///"
+password = base64.b64decode(password_base64).decode("utf-8")
+rec_email = "///"
+
+subject = "AVISO!"
+body = "Mem_server gt $RAM_PERCENTAGE_WARNING%"
+msg = f"Subject: {subject}\n\n{body}"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, password)
+server.sendmail(sender_email, rec_email, msg)
