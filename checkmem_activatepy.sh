ee190ff912be694e2b1a7d8d1bb8f9ee60566664
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-01-17 15:34:55 +0000

    Update checkmem_activatepy.sh

--- checkmem_activatepy.sh
+++ checkmem_activatepy.sh
@@ -2,7 +2,7 @@
 #---------PERSONAL VARIABLES
 PCUSER=`whoami`
 RAM_PERCENTAGE_WARNING="***ram_percentage_warning***
-HOME_DIRECTORY="/home/$PCUSER"
+SCRIPT_DIRECTORY="/home/$PCUSER.scriptmail-dependencies/"
 TMP_DIRECTORY="/tmp"
 #---------SCRIPT - CHECK MEM
 USED_MEM=`vmstat -s |awk '/used/ && /memory/' |cut -d'K' -f1 | sed "s/ //g"`
@@ -11,7 +11,7 @@ RATIO_WARNING=`echo "scale=2; $USED_MEM / $PC_MEM * 100" | bc | cut -d'.' -f1`
 #--------SCRIPT - SEND EMAIL       
 if [[ "$RATIO_WARNING" -ge $RAM_PERCENTAGE_WARNING  && ! -f $TMP_DIRECTORY/nowarning.txt ]]
 then
-	cd $HOME_DIRECTORY/.scriptmail-dependencies/
+	cd $SCRIPT_DIRECTORY
 	python3 mailwarningmem.py
 	cd $TMP_DIRECTORY
 	echo 0 > nowarning.txt
