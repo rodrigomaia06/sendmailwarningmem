c838ecb795c65a99c0c626d2d916a969137ecd03
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-01-19 22:19:11 +0000

    Add files via upload

---
+++ versions/1.0/installscript.sh
@@ -0,0 +1,36 @@
+#--------SCRIPT - SEND EMAIL
+#!/bin/bashPCUSER=`whoami`
+
+echo "Updating Packages ......"; sudo apt update &> /dev/null
+sudo apt install bc &> /dev/null
+echo "Installing Dependencies ......"; sudo apt install python3 &> /dev/null
+
+PCUSER=`whoami`
+SCRIPTDIR="/home/$PCUSER/.scriptmail-dependencies"
+mkdir $SCRIPTDIR
+cd $SCRIPTDIR
+
+wget https://raw.githubusercontent.com/VladimirBromley0750/sendmailwarningmem/main/checkmem_activatepy.sh &> /dev/null
+echo "Downloading Needed Files ......";wget https://raw.githubusercontent.com/VladimirBromley0750/sendmailwarningmem/main/sendmailwarningmem.py &> /dev/null
+
+chmod +x checkmem_activatepy.sh
+chmod +x sendmailwarningmem.py
+
+sed -i 's,###script_directory###,'"$SCRIPTDIR"',' checkmem_activatepy.sh
+
+read -p "Warning Percentage: " -r
+sed -i 's,###ram_percentage_warning###,'"$REPLY"',' checkmem_activatepy.sh
+sed -i 's,###percentage_warning###,'"$REPLY"',' sendmailwarningmem.py
+
+read -p "Sender's Gmail:  " -r
+sed -i 's,###sender_email###,'"$REPLY"',' sendmailwarningmem.py
+
+read -p "Sender's Gmail Password:  " -r
+PASSWORD_BASE64B=$(echo -n $REPLY | base64)
+sed -i 's,###password_base64###,'"$PASSWORD_BASE64B"',' sendmailwarningmem.py
+
+read -p "Recipient Email:  " -r
+sed -i 's,###rec_email###,'"$REPLY"',' sendmailwarningmem.py
+
+echo "Setup Complete!"
+sleep 1
