1bcfb4341abf2e7f5189ed79f3899b76a4c7bb68
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-02-12 01:34:25 +0000

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PCUSER=`whoami`
-echo "Removing Cronjob ......"; crontab -l | grep -v '.scriptmail-dependencies/checkmem_activatepy.sh' | crontab -; sleep 1
-echo "Removing Script's Folder ......"; rm -r -f /home/$PCUSER/.scriptmail-dependencies/; sleep 1
-echo "Uninstallation Completed!" && sleep 1
